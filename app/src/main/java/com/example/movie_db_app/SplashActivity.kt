5237fde80630152e64a409d949babe07b6816259
Stefan <vucanstefan@gmail.com>
2022-03-19 11:17:07 +0100

    fix: Refactored splash screen to use coroutines

--- app/src/main/java/com/example/movie_db_app/SplashActivity.kt
+++ app/src/main/java/com/example/movie_db_app/SplashActivity.kt
@@ -3,13 +3,29 @@ package com.example.movie_db_app
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
 
+
+    val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        setContentView(R.layout.activity_splash)
+
+        activityScope.launch {
+            delay(3000)
+
+            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
     }
 
     // TODO Add all required formats for splash image according to guidelines
