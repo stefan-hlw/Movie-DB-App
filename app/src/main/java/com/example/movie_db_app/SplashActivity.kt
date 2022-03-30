2b65ac01577817f3e4393943c818c258042843bf
m.rizabegovic <m.rizabegovic@levi9.com>
2022-03-30 12:53:19 +0200

    Ispravke na splash activity-u

--- app/src/main/java/com/example/movie_db_app/SplashActivity.kt
+++ app/src/main/java/com/example/movie_db_app/SplashActivity.kt
@@ -4,52 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.movie_db_app.ui.MovieDetails.MovieDetailsViewModel
 import kotlinx.coroutines.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : AppCompatActivity() {
 
-
-    private val activityScope = CoroutineScope(Dispatchers.Main)
     private val splashViewModel by viewModel<SplashViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        activityScope.launch {
-            delay(3000)
+       lifecycleScope.launch{
+           splashViewModel.operation.await()
+       }
 
-            setObservers()
-
-            splashViewModel.getGenresFromApi()
-            splashViewModel.getTrendingMovies()
-            splashViewModel.mapGenres()
-
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        val intent = Intent(this@SplashActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
 
     }
 
-    private fun setObservers() {
-        splashViewModel.genresMap.observe(this, Observer {
-            if(it.isNotEmpty()) {
-                splashViewModel.saveMappedGenres()
-            }
-        })
-
-        splashViewModel.genresData.observe(this, Observer {
-            if(it != null) {
-                splashViewModel.insertGenresIntoDb()
-            }
-        })
-    }
-
-    override fun onPause() {
-        activityScope.cancel()
-        super.onPause()
-    }
 }
